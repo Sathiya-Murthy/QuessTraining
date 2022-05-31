d4a2962790a7441dda53975f2ac29c1090deb7d6
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-05-31 20:38:51 +0530

    [UPADTE] Reverse of three digit number with out looping

--- src/Q3ReverseANumber.java
+++ src/Q3ReverseANumber.java
@@ -2,20 +2,13 @@ import java.util.Scanner;
 
 public class Q3ReverseANumber {
     public static void main(String[] args) {
-        int number;
+        String str;
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter an three digit number: ");
-        number=sc.nextInt();
-        if(number>99 && number<1000) {
-            System.out.print("The reverse of the number: ");
-            String str = Integer.toString(number);
-            for (int i = str.length(); 0 < i; --i) {
-                System.out.print(str.charAt(i-1));
-            }
-        }
-        else
-            System.out.println("Enter three digit number.");
-
+        str=sc.nextLine();
+        StringBuffer stringBuffer= new StringBuffer();
+        stringBuffer.append(str);
+        System.out.println("Reverse number: "+stringBuffer.reverse());
 
         }
     }
