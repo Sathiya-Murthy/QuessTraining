8839fd35c107fe50f629352d9c4f634070eeb9eb
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-05-31 19:53:16 +0530

    [ADD] Simple Interest

---
+++ src/Q2SimpleInterest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Q2SimpleInterest {
+    public static void main(String[] args) {
+        int Principle,Years;
+        double Interest=7.5;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Principle Amount :");
+        Principle=sc.nextInt();
+        System.out.print("Enter the time period in years :");
+        Years=sc.nextInt();
+        double Simple_Interest=((Double.valueOf(Principle))*(Double.valueOf(Years))*Interest)/100;
+        System.out.println("Simple Interest= "+Simple_Interest);
+        double Final_Amount=Principle+Simple_Interest;
+        System.out.println("Final Amount to be paid= "+Final_Amount);
+
+
+    }
+}
