87ead1112858583dff3a1c5339b09ffda60e2a04
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-06-01 08:12:22 +0530

    [ADD] Greater between three number using ternary operator

---
+++ src/Q8greatestOfThree.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q8greatestOfThree {
+    public static void main(String[] args) {
+        int number1,number2,number3;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the numbers:");
+        number1=scanner.nextInt();
+        number2=scanner.nextInt();
+        number3=scanner.nextInt();
+        int Greatest=(number1>=number2)?((number1>number3)?number1:number3):((number2>number3)?number2:number3);
+        System.out.println("The Greatest number: "+Greatest);
+    }
+}
