deae490cb44ad0d2f891de5fff7c9e4cf88f757e
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-06-01 06:47:04 +0530

    [ADD] Calculate the Gross Salary

---
+++ src/Q6grossSalary.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Q6grossSalary {
+    public static void main(String[] args) {
+        int Basic,TA,DA,HRA,PF,Bonus,GrossSalary;
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the basic salary: ");
+        Basic=scanner.nextInt();
+        TA=(Basic/100)*10;
+        DA=(Basic/100)*15;
+        HRA=(Basic/100)*20;
+        PF=(Basic/100)*12;
+        if(Basic>=20000)
+        {
+            Bonus=(Basic/100)*10;
+            GrossSalary=Basic+DA+TA+HRA+Bonus-PF;
+            System.out.println("Gross Salary= "+GrossSalary);
+        }
+        else
+        {
+            Bonus=(Basic/100)*20;
+            GrossSalary=Basic+DA+TA+HRA+Bonus-PF;
+            System.out.println("Gross Salary= "+GrossSalary);
+        }
+    }
+
+}
