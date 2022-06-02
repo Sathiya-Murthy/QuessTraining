0cdece8887d3bbb27b7eac19edc2206c0555e99d
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-06-02 18:37:07 +0530

    [ADD] Find table of given number using recursion

---
+++ src/Q28multiplicationTablr.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q28multiplicationTablr {
+    public static void main(String[] args) {
+        int number, num=1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        number = scanner.nextInt();
+        Table(number, num);
+    }
+
+    static void Table(int number, int num) {
+        if(num>10)
+            return;
+        System.out.println(number + " * "+ num + " = " + number*num);
+        Table(number,num+1);
+
+    }
+}
+
+
+
+
+
