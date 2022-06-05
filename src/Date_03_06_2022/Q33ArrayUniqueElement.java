eb2861a33d91748b588501dd4c8fe80091f0b20b
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-06-05 12:12:20 +0530

    [ADD] Count a total number of unique elements in an array.

---
+++ src/Date_03_06_2022/Q33ArrayUniqueElement.java
@@ -0,0 +1,34 @@
+package Date_03_06_2022;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q33ArrayUniqueElement {
+    public static void main(String[] args) {
+        int len,count;
+        ArrayList<Integer> Array=new ArrayList<>();
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the length of array: ");
+        len=scanner.nextInt();
+        int[] array=new int[len];
+        System.out.println("Enter the element in Array");
+        for(int i=0;i<len;i++)
+        {
+            array[i]=scanner.nextInt();
+        }
+        for(int i=0;i<len;i++)
+        {
+            count=0;
+            for(int j=0;j<i;j++)    {
+                if(array[i]==array[j]) {
+                    count++;
+                    break;
+                }
+            }
+            if(count==0)
+                Array.add(array[i]);
+
+        }
+        System.out.println("Number of unique element are: "+Array.size());
+    }
+}
