eac00716b8370c8b941ed940e4b85e40ce93ab8d
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-06-06 20:07:17 +0530

    [Add] Implementation of requierment 3

--- src/Date_06_06_2022/Movie.java
+++ src/Date_06_06_2022/Movie.java
@@ -4,16 +4,32 @@ public class Movie {
     private String title;
     private String studio;
     private String rating;
-    Movie(String title,String studio,String rating) {
+     public Movie(String title,String studio,String rating) {
         this.title=title;
         this.studio=studio;
         this.rating=rating;
     }
-    Movie(String title,String studio) {
+    public Movie(String title,String studio) {
         this.title=title;
         this.studio=studio;
         this.rating="PG";
     }
+
+    public Movie[] getPG(Movie[] movie)
+    {
+        Movie[] pgMovie=new Movie[movie.length];
+        int index=0;
+        for(int i=0 ; i< movie.length;i++)  {
+            if(movie[i].rating.equals("PG"))
+            {
+                pgMovie[index]=movie[i];
+                index++;
+            }
+        }
+        return pgMovie;
+    }
+
+
 
 }
 
