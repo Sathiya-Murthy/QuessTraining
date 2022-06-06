7874a26e2d276036a15dab0cd24ca32b8f16abd0
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-06-06 12:40:43 +0530

    [Add] Find whether a given number is a probable topper number or not

---
+++ src/Date_06_06_2022/Q37topperNumber.java
@@ -0,0 +1,25 @@
+package Date_06_06_2022;
+
+import java.util.Scanner;
+
+public class Q37topperNumber {
+    public static void main(String[] args) {
+        int number,oddNumber=0,evenNumber=0;
+        Scanner scanner=new Scanner(System.in);
+        number=scanner.nextInt();
+        String Number=Integer.toString(number);
+        for(int i=0;i<Number.length();i++)  {
+            if(((Character.getNumericValue(Number.charAt(i))) % 2) == 0) {
+                evenNumber+=Character.getNumericValue(Number.charAt(i));
+            }
+            if(((Character.getNumericValue(Number.charAt(i))) % 2) != 0) {
+                oddNumber+=Character.getNumericValue(Number.charAt(i));
+            }
+
+        }
+        if(oddNumber==evenNumber)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
