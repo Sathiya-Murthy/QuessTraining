27bb34cb6280a0d1ee57cd120fefb0212b42e40a
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-06-08 05:13:18 +0530

    [Add] Implementation of exception handleing

---
+++ src/Date_07_06_2022/ExceptionsHandleing.java
@@ -0,0 +1,23 @@
+package Date_07_06_2022;
+
+import java.util.Scanner;
+
+public class ExceptionsHandleing {
+    public static void main(String[] args) {
+        int x,y;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of x and y:");
+        try{
+        x=scanner.nextInt();
+        y=scanner.nextInt();
+        int result=x/y;
+            System.out.println("Result: "+result);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e);
+        }
+         catch (Exception e) {
+             System.out.println(e+" Enter any 32bit signed Integer.");
+        }
+    }
+}
