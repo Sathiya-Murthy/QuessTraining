24a306d312e186ca17b6377501aa7e9c7063fdfb
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-06-09 06:31:02 +0530

    [Add] Implementation of string replace with 0 and 1

--- src/Date_08_06_2022/Q42StringReplace.java
+++ src/Date_08_06_2022/Q42StringReplace.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Q42StringReplace {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the string: ");
         String inputString;
